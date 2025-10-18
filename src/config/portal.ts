90de40fa44999f2d3d2f2b395912d6f9f826a2ef
thefirsthero <vukosi90@gmail.com>
2025-10-18 21:31:00 +0200

    Added my other sites

--- src/config/portal.ts
+++ src/config/portal.ts
@@ -18,4 +18,22 @@ export const portal: Portal[] = [
     href: "https://calchub.coraxi.com/",
     image: "/images/calchub.png",
   },
+  {
+    title: "Confessions",
+    description: "A place to share your confessions anonymously.",
+    href: "https://confess.coraxi.com/",
+    image: "/images/confessions.png",
+  },
+  {
+    title: "Couples Games",
+    description: "A collection of games for couples (or any 2 people).",
+    href: "https://games.coraxi.com/",
+    image: "/images/couples-games.png",
+  },
+  {
+    title: "Pomodoro Focus",
+    description: "A simple Pomodoro timer to boost your productivity.",
+    href: "https://pomodoro.coraxi.com/",
+    image: "/images/pomodoro.png",
+  },
 ];
